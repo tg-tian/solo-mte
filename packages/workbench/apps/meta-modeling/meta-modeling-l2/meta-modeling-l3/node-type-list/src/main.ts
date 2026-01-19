3b19e1c921918324bc91345ac83b467cd2e256e9
zt <Icaruz@example.org>
2026-01-19 15:12:10 +0800

    feat: 创建设备型号、节点类型、组件类型应用入口
    - 创建device-model-list独立应用入口
    - 创建node-type-list独立应用入口
    - 创建component-list独立应用入口

---
+++ packages/workbench/apps/meta-modeling/meta-modeling-l2/meta-modeling-l3/node-type-list/src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import NodeTypeList from '../../meta-modeling-app/src/views/meta/NodeTypeList/index.vue';
+import '../../meta-modeling-app/src/assets/style.css';
+
+const app = createApp(NodeTypeList);
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
+app.use(pinia);
+app.use(ElementPlus);
+
+app.mount('#app');
