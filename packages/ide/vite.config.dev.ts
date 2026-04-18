8857a502c1b7c5f18b457fc25fa1bd92d973d06f
tgapk <914837973@qq.com>
2026-04-18 16:31:27 +0800

    fix:修复路径重写错误

--- packages/ide/vite.config.dev.ts
+++ packages/ide/vite.config.dev.ts
@@ -161,7 +161,8 @@ export default defineConfig({
             "/workbench": {
                 target: "http://127.0.0.1:8080",
                 changeOrigin: true,
-                secure: false
+                secure: false,
+                rewrite: (path) => path.replace(/^\/workbench/, "")
             },
         }
     },
